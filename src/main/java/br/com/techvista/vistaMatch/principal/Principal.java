db809886053f104665be7e1c863b617a2c3b3b3e
Renato Carvalho <rsilv1822@gmail.com>
2025-11-19 18:02:35 -0300

    lambda functions

--- src/main/java/br/com/techvista/vistaMatch/principal/Principal.java
+++ src/main/java/br/com/techvista/vistaMatch/principal/Principal.java
@@ -3,12 +3,15 @@ package br.com.techvista.vistaMatch.principal;
 import br.com.techvista.vistaMatch.model.DadosEpisodioModel;
 import br.com.techvista.vistaMatch.model.DadosSerieModel;
 import br.com.techvista.vistaMatch.model.DadosTemporadaModel;
+import br.com.techvista.vistaMatch.model.EpisodioModel;
 import br.com.techvista.vistaMatch.service.ConsumoAPI;
 import br.com.techvista.vistaMatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Principal {
 
@@ -43,16 +46,63 @@ public class Principal {
 
         temporadas.forEach(System.out::println);
 
-//        for (int i = 0; i <= dadosSerie.totalTemporadas(); i++) {
-//
-//            List<DadosEpisodioModel> episodiosTemporada = temporadas.get(i).episodios();
-//            for(int j = 0; j < episodiosTemporada.size(); j++) {
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
+        for (int i = 0; i < temporadas.size(); i++) {
+
+            List<DadosEpisodioModel> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
+        /*System.out.println("=================================================================================");
+
+        List<String> nomes = Arrays.asList("Renato", "Mayra", "Cinthya", "Gabriel", "Ezequiel", "Carol");
+
+        nomes.stream()
+                .sorted() // Organiza os itens de 'nomes' em ordem alfabética
+                //.limit(2) // Limita a quantidade de itens em 2
+                .filter(n -> n.length() == 5) // Filtra nomes com 5 exatos caracteres
+                .map(n -> n.toUpperCase())
+                .forEach(System.out::println);*/
+
+        List<DadosEpisodioModel> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .toList(); // Lista imutavel, não permite adicionar ou remover itens, para alterar, use "toList()"
+
+        System.out.println("\n Top 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodioModel::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        System.out.println("=========================================================================");
+
+        List<EpisodioModel> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new EpisodioModel(t.numeroTemporada(), d))
+                )
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("=========================================================================");
+        System.out.println("A partir de que ano você quer assistir os episódios?");
+        var ano = sc.nextInt();
+        sc.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episodio: " + e.getTitulo() +
+                                " Data de lançamento: " + e.getDataLancamento().format(formatter)
+                ));
     }
 
 }
